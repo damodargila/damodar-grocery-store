1a34aa0ba28f4580a05b37bc31c2a1f535e63bdf
Damodar Gila <damodargila01@gmail.com>
2026-06-23 16:15:34 +0530

    Fix add product postgres insert

--- app.py
+++ app.py
@@ -813,7 +813,7 @@ def add_product():
             """
             INSERT INTO products
             (name, category, price, image, stock, description, image2, image3, image4, image5)
-            VALUES (?,?,?,?,?,?,?,?,?,?)
+            VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
             """,
             (
                 name,
